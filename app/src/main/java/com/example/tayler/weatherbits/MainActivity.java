76f7409a09fd0d0925d761868ed07476905fd34a
Tayler <taylernbray@gmail.com>
2017-06-08 18:21:44 -0400

    Add OkHTTP and ButterKnife

--- app/src/main/java/com/example/tayler/weatherbits/MainActivity.java
+++ app/src/main/java/com/example/tayler/weatherbits/MainActivity.java
@@ -2,14 +2,61 @@ package com.example.tayler.weatherbits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView) TextView textView;
+
+    private static final String DARK_SKY_KEY = BuildConfig.DARK_SKY_KEY;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //testing
+
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.darksky.net/forecast/" + DARK_SKY_KEY + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                try {
+                    if (response.isSuccessful()){
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
     }
 }
